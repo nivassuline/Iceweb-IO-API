391d78f37e3e2b3af173bd767fe12a1f671dfbad
nivassuline <assulineniv@gmail.com>
2023-11-30 19:34:51 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1507,10 +1507,10 @@ def get_data():
     if filter_query is None:
         if full_data:
             if data_type == 'users':
-                query = text(f"SELECT DISTINCT ON (full_name) (full_name,email,maid,maid_os) FROM {company_id} WHERE {date_range_query};")
+                query = text(f"SELECT DISTINCT ON (full_name) full_name,email,maid,maid_os FROM {company_id} WHERE {date_range_query};")
                 with ENGINE.connect() as connection:
                     data = connection.execute(query)
-                    result = [dict(zip('full_name','email','maid','maid_os', row)) for row in data]
+                    result = [dict(zip(['full_name','email','maid','maid_os'], row)) for row in data]
                 
                 return jsonify(result)
             else:
@@ -1946,7 +1946,6 @@ def internal_error(error):
     print(error)
 
     return jsonify("Not Found")
-
 
 
 # def update_excluded_users(segment_id):
