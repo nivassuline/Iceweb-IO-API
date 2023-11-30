ab51402307fc6f0773eb180764766ad35ffbc95d
nivassuline <assulineniv@gmail.com>
2023-11-30 19:09:03 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1507,7 +1507,7 @@ def get_data():
     if filter_query is None:
         if full_data:
             if data_type == 'users':
-                query = text(f"SELECT DISTINCT ON (full_name) * FROM {company_id} WHERE {date_range_query};")
+                query = text(f"SELECT DISTINCT ON (full_name) (full_name,email,maid,maid_os) FROM {company_id} WHERE {date_range_query};")
             else:
                 query = text(f"SELECT  * FROM {company_id} WHERE {date_range_query};")
         else:
