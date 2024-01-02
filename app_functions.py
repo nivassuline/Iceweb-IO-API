caf28b6f3ac0eee3bcdd00d225ffb475ba13bf7d
nivassuline <assulineniv@gmail.com>
2024-01-02 15:57:16 +0200

    Update app_functions.py

--- app_functions.py
+++ app_functions.py
@@ -6,7 +6,6 @@ import re
 from datetime import datetime
 import secrets
 from sqlalchemy import text
-from flask_socketio import SocketIO
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 import threading
